7a34bc11f54605f16b32eaf32b5a491ac2a9b13e
austin1052 <austin1052@gmail.com>
2023-02-02 05:02:14 -0500

    update router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Navbar, DesktopNav, Hero, Footer, ScrollToTop, Geofilters } from "./components";
 import { About, Projects } from "./pages";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom'
 import "./App.css";
 
 function App() {
@@ -20,37 +21,21 @@ function App() {
 
   return (
     <Router>
-      <ScrollToTop />
-      <>
-        {mobileDisplay ? (
-          <Navbar setMenuOpen={setMenuOpen} menuOpen={menuOpen} />
-        ) : (
-          <DesktopNav />
-        )}
-        <div className={menuOpen ? "hidden content" : "content"}>
-          <Switch>
-            <Route path="/" exact>
-              <Hero />
-            </Route>
-          </Switch>
-          <Switch>
-            <Route path="/about" exact>
-              <About />
-            </Route>
-          </Switch>
-          <Switch>
-            <Route path="/portfolio" exact>
-              <Projects />
-            </Route>
-          </Switch>
-          <Switch>
-            <Route path="/geofilters" exact>
-              <Geofilters />
-            </Route>
-          </Switch>
-        </div>
-        <Footer />
-      </>
+      {mobileDisplay ? (
+        <Navbar setMenuOpen={setMenuOpen} menuOpen={menuOpen} />
+      ) : (
+        <DesktopNav />
+      )}
+      <div className={menuOpen ? "hidden content" : "content"}>
+        <Routes>
+          {/* <ScrollToTop /> */}
+          <Route path="/" element={<Hero />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/portfolio" element={<Projects />} />
+          <Route path="/geofilters" element={<Geofilters />} />
+        </Routes>
+      </div>
+      <Footer />
     </Router>
   );
 }
