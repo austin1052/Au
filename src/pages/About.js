ad18172a2a764173e1ca5ba0e06df3d5a33fb853
austin1052 <austin1052@gmail.com>
2023-02-03 16:31:22 -0500

    About page style updates

--- src/pages/About.js
+++ src/pages/About.js
@@ -18,16 +18,32 @@ const About = () => {
                 I am a Frontend Web Developer based in Brooklyn, NY who has a passion for creating amazing user experiences and web applications that make life easier and more fun.
                 I have a strong background in customer service and team management.
               </p> */}
-              <p> Two things </p>
-              <p>1. I have always loved arts and crafts.</p>
-              <p> 2. My favorite class in school was chemistry.</p>
-              <p>I love building something beautiful, and I love solving problems; web development is a perfect marriage of the two. </p>
+              <p>
+                {`Two things:
+1.   I have always loved arts and crafts.
+2. My favorite class in school was chemistry.`}
+              </p>
 
-              <p>I am a graduate of Fullstack Academy where I learned the skills to build awesome web applications using Javascript, CSS, React, Node.js, PostgreSQL, and more. I am always looking to learn and become a better developer. I am open to career and networking opportunities and appreciate any opportunity to connect. </p>
+              <p>
+                I love building something beautiful, and I love solving problems; web development is a perfect marriage of the two.
+              </p>
 
-              <p>I have spent most of my professional life working retail management at Wegmans Food Markets and it taught me the most important part of running a successful business; caring for your employees and showing the same respect to your day one employees as you do your CEO. </p>
+              <p>
+                I am a graduate of Fullstack Academy where I learned the skills to build awesome web applications using Javascript, CSS, React, Node.js, PostgreSQL, and more. I am always looking to learn and become a better developer.
+              </p>
 
-              <p>I have experience leading large teams, hiring and training new employees, and managing a store that handles millions in sales every week. I am comfortable working in an environment where plans change often, new ideas are necessary, and input is gladly taken from any employee.</p>
+              <p>
+                I have spent most of my professional life working retail management at Wegmans Food Markets and it taught me the most important part of running a successful business; caring for your employees and showing the same respect to your day one employees as you do your CEO.
+              </p>
+
+              <p>
+                I have experience leading large teams, hiring and training new employees, and managing a store that handles millions in sales every week. I am comfortable working in an environment where plans change often, new ideas are necessary, and input is gladly taken from any employee.
+              </p>
+
+              <p>
+                I am open to career and networking opportunities and appreciate any opportunity to connect.
+              </p>
+
               <p>
                 Contact me. Let's build something!
               </p>
